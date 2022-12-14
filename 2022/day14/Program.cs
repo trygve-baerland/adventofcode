9a403859e40bbde5ae304b930eb9b19840e56a9c
Trygve Bærland <trygve.baerland@hafslundeco.no>
2022-12-14 13:01:20 +0100

    Part 1 and 2 together.

--- 2022/day14/Program.cs
+++ 2022/day14/Program.cs
@@ -1,21 +1,38 @@
-﻿using Day14;
+﻿using System.Diagnostics;
+using Day14;
 using Utils;
 
+var sw = new Stopwatch();
 // Parse/prepare input:
+sw.Start();
 var rockLines = File.OpenText("input.txt")
     .GetLines()
     .Select(Parser.GetRockLine)
     .Select(rockLine => rockLine.ToList())
     .ToList();
 
-Cave cave = new(rockLines, withFloor: true);
+Console.WriteLine($"Parsing took {sw.ElapsedMilliseconds} ms");
 
 // Part 1:
+sw.Restart();
+Cave cave = new(rockLines, withFloor: false);
 var source = new Node { X = 500, Y = 0 };
 int counter = 0;
 while (cave.DropSand(source) != BlockedEnum.Abyss)
 {
     counter++;
 }
-cave.Print();
-Console.WriteLine($"Part 1: {counter}");
+//cave.Print();
+Console.WriteLine($"Part 1: {counter} [{sw.ElapsedMilliseconds}]");
+
+// Part 2:
+sw.Restart();
+cave = new(rockLines, true);
+counter = 0;
+while (cave.DropSand(source) != BlockedEnum.Abyss)
+{
+    counter++;
+}
+//cave.Print();
+Console.WriteLine($"Part 2: {counter} [{sw.ElapsedMilliseconds}]");
+
