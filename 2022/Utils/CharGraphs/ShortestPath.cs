c81f8acf91de4b857fbc7e699e1b3ad79369665c
Trygve Bærland <trygve.baerland@hafslundeco.no>
2022-12-16 22:37:14 +0100

    Closest until now...

--- 2022/Utils/CharGraphs/ShortestPath.cs
+++ 2022/Utils/CharGraphs/ShortestPath.cs
@@ -89,7 +89,7 @@ public static class ShortestPath
         return int.MaxValue;
     }
 
-    private static int CheckedSum(int a, int b)
+    public static int CheckedSum(int a, int b, int defaultValue = int.MaxValue)
     {
         try
         {
@@ -97,7 +97,114 @@ public static class ShortestPath
         }
         catch (OverflowException)
         {
-            return int.MaxValue;
+            return defaultValue;
         }
+    }
+
+    public static ShortestPathMapping<TNode> FloydWarshall<TNode>(
+        IEnumerable<TNode> nodes,
+        Func<TNode, IEnumerable<TNode>> adjacentNodes)
+    where TNode : notnull
+    {
+        var result = new ShortestPathMapping<TNode>(nodes, adjacentNodes);
+        result.FloydWarshall();
+        return result;
+    }
+}
+
+public class IndexMapping<TNode>
+where TNode : notnull
+{
+    public Dictionary<TNode, int> Mapping { get; private set; }
+    public IndexMapping(IEnumerable<TNode> nodes)
+    {
+        Mapping = nodes.Select((node, i) => (node, i))
+            .ToDictionary(
+                item => item.node,
+                item => item.i
+            );
+    }
+    public int Get(TNode node) => Mapping[node];
+    public bool TryGet(TNode node, out int value) => Mapping.TryGetValue(node, out value);
+    public int NumNodes { get => Mapping.Count; }
+
+    public TNode FromIndex(int i)
+    {
+        if (i < 0 || i >= NumNodes)
+        {
+            throw new Exception($"Invalid index {i}");
+        }
+        return Mapping.Where(item => item.Value == i)
+            .Select(item => item.Key)
+            .First();
+    }
+}
+
+public class ShortestPathMapping<TNode>
+where TNode : notnull
+{
+    public IndexMapping<TNode> Index { get; private set; }
+    public int[,] Distances { get; private set; }
+
+    public int Size { get => Index.NumNodes; }
+
+    public ShortestPathMapping(IEnumerable<TNode> nodes, Func<TNode, IEnumerable<TNode>> adjacentNodes)
+    {
+        Index = new IndexMapping<TNode>(nodes);
+        Distances = new int[Size, Size];
+        // Initialize with int.MaxValue:
+        foreach (var i in Enumerable.Range(0, Size))
+        {
+            foreach (var j in Enumerable.Range(0, Size))
+            {
+                Distances[i, j] = int.MaxValue;
+            }
+            Distances[i, i] = 0;
+        }
+        // Add edges:
+        foreach (var u in nodes)
+        {
+            int i = Index.Get(u);
+            foreach (var v in adjacentNodes(u))
+            {
+                int j = Index.Get(v);
+                Distances[i, j] = 1;
+            }
+        }
+    }
+
+    public void FloydWarshall()
+    {
+        foreach (var k in Enumerable.Range(0, Size))
+        {
+            foreach (var i in Enumerable.Range(0, Size))
+            {
+                foreach (var j in Enumerable.Range(0, Size))
+                {
+                    var candidate = ShortestPath.CheckedSum(Distances[i, k], Distances[k, j]);
+                    if (candidate < Distances[i, j])
+                    {
+                        Distances[i, j] = candidate;
+                    }
+                }
+            }
+        }
+    }
+
+    public int GetDistance(TNode source, TNode target)
+    {
+        return Distances[
+            Index.Get(source),
+            Index.Get(target)
+        ];
+    }
+
+    public IEnumerable<(TNode node, int dist)> GetFrom<TKey>(TNode node, Func<TNode, TKey> selector)
+    {
+        var i = Index.Get(node);
+        return Enumerable.Range(0, Size)
+            .Where(j => j != i)
+            .Select(j => (Index.FromIndex(j), Distances[i, j]))
+            .OrderBy(item => selector(item.Item1));
     }
 }
--- 2022/day16/TunnelSystem.cs
+++ 2022/day16/TunnelSystem.cs
@@ -1,3 +1,4 @@
+using Utils.Graphs;
 namespace Day16;
 
 public class Valve
@@ -7,12 +8,88 @@ public class Valve
 
     public List<string> ConnectedValves { get; } = new List<string>();
 
-    public string ToString()
+    public override string ToString()
     {
         string result = $"('{Name}', ";
         result += $"{Rate}, [";
         result += string.Join(',', ConnectedValves);
         result += "])";
         return result;
+    }
+}
+
+public static class TunnelHelpers
+{
+    public static List<(Valve valve, int time)> OptimalRelease(
+        Valve source,
+        ShortestPathMapping<Valve> shortestPathMapping,
+        int timeout
+    )
+    {
+        // Initialize stuff:
+        var stack = new Stack<(Valve node, IEnumerator<(Valve, int)> neighbours, int time, int total, List<(Valve valve, int time)> visited)>();
+        stack.Push(
+            (
+                source,
+                shortestPathMapping.GetFrom(source, item => -item.Rate).Where(item => item.node.Rate > 0).GetEnumerator(),
+                0,
+                0,
+                new List<(Valve valve, int time)>() {
+                    (source, 0)
+                }
+            )
+        );
+        var totals = new List<List<(Valve valve, int time)>>();
+        while (stack.Count > 0)
+        {
+            var (v, neighbours, time, total, visited) = stack.Peek();
+            var last = visited.Last();
+            if (v.Name != last.valve.Name)
+            {
+                throw new Exception($"Data inconsistency in name {v.Name} != {last.valve.Name}");
+            }
+            if (time != last.time)
+            {
+                throw new Exception($"Data inconsistency in time {time} != {last.time}");
+            }
+            if (neighbours.MoveNext())
+            {
+                // We are considering going to w next:
+                var (w, dist) = neighbours.Current;
+                var newTime = time + dist + 1; // Going there and turning it on.
+                if (newTime < timeout && !visited.Select(item => item.valve.Name).Contains(w.Name)) // We only care if can get there in time
+                {
+                    var newVisited = new List<(Valve valve, int time)>(visited) // I've been promised this makes a copy...
+                    {
+                        (w, newTime)
+                    };
+                    var newTotal = total + (timeout - newTime) * w.Rate;
+                    stack.Push(
+                        (
+                            node: w,
+                            neighbours: shortestPathMapping.GetFrom(source, item => -item.Rate).Where(item => item.node.Rate > 0).GetEnumerator(),
+                            time: newTime,
+                            total: newTotal,
+                            visited: newVisited
+                        )
+                    );
+                }
+            }
+            else
+            {
+                var item = stack.Pop();
+                totals.Add(item.visited);
+            }
+        }
+        // Get best path:
+        Console.WriteLine($"Searched {totals.Count} paths");
+        var pathVals = totals.Select((item, i) => EvaluatePath(item, timeout)).ToList();
+        var index = pathVals.IndexOf(pathVals.Max());
+        return totals[index];
+    }
+
+    public static int EvaluatePath(IEnumerable<(Valve valve, int time)> visited, int timeout)
+    {
+        return visited.Select(item => (timeout - item.time) * item.valve.Rate).Sum();
     }
 }
