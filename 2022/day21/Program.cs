9b69b054fbaaa367f999dd113dcdf0e98d81fd5c
Trygve Bærland <trygve.baerland@hafslundeco.no>
2022-12-22 21:22:47 +0100

    Day 21 part 2.

--- 2022/day21/Program.cs
+++ 2022/day21/Program.cs
@@ -11,3 +11,23 @@ File.OpenText("input.txt")
 // Part 1:
 var part1 = ShoutingMonkey.AllMonkeys["root"].Value;
 Console.WriteLine($"Part 1: {part1}");
+
+// Part 2:
+// Get trace to "humn":
+var root = ShoutingMonkey.AllMonkeys["root"];
+var trace = root.Find("humn");
+
+Console.WriteLine(string.Join(", ", trace));
+
+// Main loop:
+var monkey = root;
+monkey.Operation = Op.Compare;
+decimal target = 0;
+foreach (var dir in trace)
+{
+    // Invert current monkey:
+    target = monkey.Invert(dir, target);
+    monkey = monkey.GetChild(dir);
+}
+Console.WriteLine(monkey);
+Console.WriteLine($"Part 2: {target}");
--- 2022/day21/ShoutingMonkey.cs
+++ 2022/day21/ShoutingMonkey.cs
@@ -5,7 +5,14 @@ public enum Op
     Plus,
     Minus,
     Multiply,
-    Divide
+    Divide,
+    Compare,
+}
+
+public enum Direction
+{
+    L,
+    R
 }
 
 public class ShoutingMonkey
@@ -31,6 +38,7 @@ public class ShoutingMonkey
                     Op.Minus => lhs.Value - rhs.Value,
                     Op.Multiply => lhs.Value * rhs.Value,
                     Op.Divide => lhs.Value / rhs.Value,
+                    Op.Compare => Convert.ToDecimal(lhs.Value == rhs.Value),
                     _ => throw new Exception($"Unsupported operation {Operation}")
                 };
             }
@@ -51,4 +59,93 @@ public class ShoutingMonkey
     }
     #endregion Constructor
     public static Dictionary<string, ShoutingMonkey> AllMonkeys { get; } = new();
+
+    #region Misc Helpers
+    public IEnumerable<ShoutingMonkey> GetChildren()
+    {
+        if (Lhs is not null && Lhs != "")
+            yield return AllMonkeys[Lhs];
+        if (Rhs is not null && Rhs != "")
+            yield return AllMonkeys[Rhs];
+    }
+
+    public ShoutingMonkey GetChild(Direction dir) => dir switch
+    {
+        Direction.L => AllMonkeys[Lhs!],
+        Direction.R => AllMonkeys[Rhs!],
+        _ => throw new Exception($"Unsupported direction '{dir}'")
+    };
+
+    public List<Direction> Find(string name)
+    {
+        var stack = new Stack<(ShoutingMonkey monkey, IEnumerator<ShoutingMonkey> children, List<Direction> trace)>();
+        stack.Push((this, GetChildren().GetEnumerator(), new List<Direction>()));
+
+        while (stack.Count > 0)
+        {
+            var (monkey, children, trace) = stack.Peek();
+            if (children.MoveNext())
+            {
+                var next = children.Current;
+                Direction dir;
+                if (next.Name == monkey.Lhs) dir = Direction.L;
+                else if (next.Name == monkey.Rhs) dir = Direction.R;
+                else throw new Exception($"Unable to determine side of expression!");
+                var nextTrace = new List<Direction>(trace)
+                {
+                    dir
+                };
+                stack.Push((next, next.GetChildren().GetEnumerator(), nextTrace));
+            }
+            else
+            {
+                // Visit monkey
+                var item = stack.Pop();
+                if (item.monkey.Name == name) return item.trace;
+            }
+        }
+        throw new Exception($"Unable to find '{name}' from '{Name}'");
+    }
+
+    /// <summary>
+    /// If we're at an expression (lhs op rhs) find the value that lhs or rhs needs to be so that the
+    /// this node's value becomes target
+    /// </summary>
+    public decimal Invert(Direction dir, decimal target)
+    {
+        if (Lhs is null || Rhs is null || Operation is null) throw new Exception($"Cannot invert a leaf node.");
+        var oppVal = dir switch
+        {
+            Direction.L => AllMonkeys[Rhs].Value,
+            Direction.R => AllMonkeys[Lhs].Value,
+            _ => throw new Exception($"Unsupported direction '{dir}'")
+        };
+
+        return Operation switch
+        {
+            Op.Plus => target - oppVal,
+            Op.Multiply => target / oppVal,
+            Op.Minus => dir switch
+            {
+                Direction.L => target + oppVal,
+                Direction.R => oppVal - target,
+                _ => throw new Exception($"Unsupported direction '{dir}'")
+            },
+            Op.Divide => dir switch
+            {
+                Direction.L => target * oppVal,
+                Direction.R => oppVal / target,
+                _ => throw new Exception($"Unsupported direction '{dir}'")
+            },
+            Op.Compare => oppVal,
+            _ => throw new Exception($"Cannot invert operation '{Operation}'")
+        };
+    }
+
+    public override string ToString()
+    {
+        string result = $"Monkey {Name}({Value})";
+        return result;
+    }
+    #endregion Misc Helpers
 }
