9c7ca91444fb55717a89f2199fb16374690b3cd2
Trygve Bærland <trygve.baerland@hafslundeco.no>
2022-12-25 18:52:46 +0100

    Day 25 part 1, and only part 1.

---
+++ 2022/day25/Program.cs
@@ -0,0 +1,10 @@
+﻿using Utils;
+using Day25;
+
+// Part 1:
+var part1 = File.OpenText("input.txt")
+    .GetLines()
+    .Select(line => new Snafu(line).ToInt())
+    .Sum();
+
+Console.WriteLine($"Part 1: {part1} or {Snafu.FromInt(part1)}");
---
+++ 2022/day25/Snafu.cs
@@ -0,0 +1,70 @@
+using Utils;
+
+namespace Day25;
+
+public class Snafu
+{
+    public IEnumerable<char> Digits { get; set; } = new List<char>();
+
+    public Snafu(string text)
+    {
+        Digits = text.ToList();
+    }
+
+    public long ToInt()
+    {
+        // Start with the lowest Digit:
+        return Digits.Reverse()
+            .Aggregate<char, (long total, long denom), long>((total: 0, denom: 1),
+                (acc, item) => (checked(acc.total + CharValue(item) * acc.denom), checked(acc.denom * 5)),
+                item => item.total);
+    }
+
+    public static long CharValue(char c) => c switch
+    {
+        '=' => -2,
+        '-' => -1,
+        '0' => 0,
+        '1' => 1,
+        '2' => 2,
+        _ => throw new Exception($"'{c}' is not a SNAFU digit.")
+    };
+
+    public static Snafu FromInt(long value)
+    {
+        // Initialize:
+        List<char> digits = new();
+        // Get number of digits:
+        while (value != 0)
+        {
+            long mod = value % 5; // 0,1,2,3,4
+            // Get snafu digit:
+            long digit = mod switch
+            {
+                <= 2 => mod,
+                _ => mod - 5
+            };
+            value -= digit; // Number should now be congruent to 0 mod 5
+            value /= 5;
+            // Add digit:
+            digits.Add(ToChar(digit));
+        }
+        digits.Reverse();
+        return new Snafu(string.Join("", digits));
+    }
+
+    public override string ToString()
+    {
+        return string.Join("", Digits);
+    }
+
+    public static char ToChar(long value) => value switch
+    {
+        -2 => '=',
+        -1 => '-',
+        0 => '0',
+        1 => '1',
+        2 => '2',
+        _ => throw new Exception($"{value} cannot be converted to a snafu digit.")
+    };
+}
