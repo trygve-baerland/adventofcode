14e09659f150a05c96005512a621d2f45a433b24
Trygve Bærland <trygve.baerland@hafslundeco.no>
2023-12-03 08:59:17 +0100

    Dag3

--- 2023/aoc/Program.cs
+++ 2023/aoc/Program.cs
@@ -7,21 +7,21 @@ Console.WriteLine( "Welcome to my AoC 2023 solutions!" );
 var solutions = System.Reflection.Assembly.GetExecutingAssembly()
   .GetTypes()
   .Where( mytype => mytype.GetInterfaces().Contains( typeof( IPuzzle ) ) )
-  .ToDictionary( mytype => mytype.Name, mytype => ( IPuzzle ) Activator.CreateInstance( mytype )! );
+  .ToDictionary( mytype => mytype.Name, mytype => mytype );
 
 Parser.Default.ParseArguments<CommandLineOptions>( args )
   .WithParsed( o => {
-      var solution = solutions[$"Day{o.Day}"];
-      Console.WriteLine( "Part 1:" );
-      solution.Part1();
-      Console.WriteLine( "Part 2:" );
-      solution.Part2();
+    var solution = ( IPuzzle ) Activator.CreateInstance( solutions[$"Day{o.Day}"] )!;
+    Console.WriteLine( "Part 1:" );
+    solution.Part1();
+    Console.WriteLine( "Part 2:" );
+    solution.Part2();
   } )
   .WithNotParsed( errs => {
-      Console.WriteLine( "You stupid piece of shit." );
-      Console.WriteLine( "Tilgjengelige løsninger er:" );
-      foreach ( var name in solutions.Keys )
-      {
-          Console.WriteLine( $"\t{name}" );
-      }
+    Console.WriteLine( "You stupid piece of shit." );
+    Console.WriteLine( "Tilgjengelige løsninger er:" );
+    foreach ( var name in solutions.Keys )
+    {
+      Console.WriteLine( $"\t{name}" );
+    }
   } );
