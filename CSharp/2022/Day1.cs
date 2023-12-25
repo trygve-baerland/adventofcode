7ebf72b883560c998602aeff699e42d53a95f4a6
Trygve Bærland <trygve.baerland@hafslundeco.no>
2023-12-25 21:48:47 +0100

    Dag 1 2022 refaktorert

---
+++ CSharp/2022/Day1.cs
@@ -0,0 +1,31 @@
+﻿using Sprache;
+using AoC.Utils;
+
+namespace AoC.Y2022;
+
+public sealed class Day1 : IPuzzle
+{
+    List<int> TestElves() => Helpers.CaloriesPerElf.Parse( string.Concat( "2022/inputdata/day1_test.txt".Stream().GetChars() ) ).ToList();
+    List<int> ActualElves() => Helpers.CaloriesPerElf.Parse( string.Concat( "2022/inputdata/day1.txt".Stream().GetChars() ) ).ToList();
+    public void Part1()
+    {
+        var elves = ActualElves();
+        Console.WriteLine( $"The elf with the most hoarded calories: {elves.Max()}" );
+    }
+
+    public void Part2()
+    {
+        var result = ActualElves().OrderByDescending( cal => cal ).Take( 3 ).Sum();
+        Console.WriteLine( $"The three richest elves have a total of {result} calories." );
+    }
+}
+public static partial class Helpers
+{
+
+    public static readonly Parser<IEnumerable<int>> CaloriesForOneElf =
+        Parse.Number.Select( int.Parse ).DelimitedBy( Parse.LineEnd.Once() );
+
+    public static readonly Parser<IEnumerable<int>> CaloriesPerElf =
+        CaloriesForOneElf.Select( cals => cals.Sum() )
+        .DelimitedBy( Parse.Char( '\n' ).AtLeastOnce() );
+}
