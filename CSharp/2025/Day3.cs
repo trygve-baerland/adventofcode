87fb988e8cedbb1bc52aab7d9fdeb12a94cbc5e9
trygve-baerland <trygve.baerland@hafslund.no>
2025-12-02 07:25:53 +0100

    Forberedelser til dag 3

---
+++ CSharp/2025/Day3.cs
@@ -0,0 +1,19 @@
+
+using AoC.Utils;
+
+namespace AoC.Y2025;
+
+public sealed class Day3 : IPuzzle
+{
+    private IEnumerable<string> TestData =
+        "2025/inputdata/day3_test.txt".GetLines();
+
+    private IEnumerable<string> Data =
+        "2025/inputdata/day3.txt".GetLines();
+
+    public void Part1() { }
+
+    public void Part2() { }
+}
+
+static partial class Helpers { }
