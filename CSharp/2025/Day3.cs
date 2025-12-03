4d00405441d1d8be282d6808d54ebb5d2794741b
trygve-baerland <trygve.baerland@hafslund.no>
2025-12-03 06:41:12 +0100

    Dag 3

--- CSharp/2025/Day3.cs
+++ CSharp/2025/Day3.cs
@@ -1,19 +1,57 @@
 
 using AoC.Utils;
+using Sprache;
 
 namespace AoC.Y2025;
 
 public sealed class Day3 : IPuzzle
 {
-    private IEnumerable<string> TestData =
-        "2025/inputdata/day3_test.txt".GetLines();
+    private IEnumerable<BatteryBank> TestData =
+        "2025/inputdata/day3_test.txt".GetLines().Select( Helpers.Battery.Parse );
 
-    private IEnumerable<string> Data =
-        "2025/inputdata/day3.txt".GetLines();
+    private IEnumerable<BatteryBank> Data =
+        "2025/inputdata/day3.txt".GetLines().Select( Helpers.Battery.Parse ).ToList();
 
-    public void Part1() { }
+    public void Part1()
+    {
+        Console.WriteLine( Data.Select( b => b.Joltage( 2 ) ).Sum() );
+    }
 
-    public void Part2() { }
+    public void Part2()
+    {
+        Console.WriteLine( Data.Select( b => b.Joltage( 12 ) ).Sum() );
+    }
 }
 
-static partial class Helpers { }
+public record struct BatteryBank( List<int> Batteries )
+{
+    public readonly override string ToString()
+    {
+        return $"Battery[{string.Join( ',', Batteries )}]";
+    }
+
+    public long Joltage( int numDigits )
+    {
+        var remDigs = numDigits;
+        long result = 0;
+        List<int> cands = Batteries;
+        while ( remDigs > 0 )
+        {
+            var maxVal = cands[..^(remDigs - 1)].Max();
+            var maxI = cands[..^(remDigs - 1)].IndexOf( maxVal );
+            // update stuff
+            remDigs -= 1;
+            result = 10 * result + maxVal;
+            cands = cands[(maxI + 1)..];
+
+        }
+        return result;
+    }
+}
+
+static partial class Helpers
+{
+    public static readonly Parser<BatteryBank> Battery =
+        Parse.Digit.Select( d => d - '0' )
+        .AtLeastOnce().Select( digs => new BatteryBank( [.. digs] ) );
+}
