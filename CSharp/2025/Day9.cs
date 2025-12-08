6f4d3a559b723dd703da1c02832dab9c92fbec4d
trygve-baerland <trygve.baerland@hafslund.no>
2025-12-08 08:48:30 +0100

    ⚡️ Forberedelser til dag 9

---
+++ CSharp/2025/Day9.cs
@@ -0,0 +1,16 @@
+
+using AoC.Utils;
+
+namespace AoC.Y2025;
+
+public sealed class Day9 : IPuzzle
+{
+    private IEnumerable<string> TestData =
+        "2025/inputdata/day9_test.txt".GetLines();
+    private IEnumerable<string> Data =
+        "2025/inputdata/day9.txt".GetLines();
+
+    public void Part1() { }
+
+    public void Part2() { }
+}
